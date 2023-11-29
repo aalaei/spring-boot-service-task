fd4c10c60fb07a46e7d1affcc1f4b807200d6b36
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-11-29 23:28:13 +0330

    API documents added

--- src/main/java/com/swisscom/tasks/task3/controller/HealthController.java
+++ src/main/java/com/swisscom/tasks/task3/controller/HealthController.java
@@ -1,12 +1,30 @@
 package com.swisscom.tasks.task3.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
+@Tag(name = "Health", description = "Health API")
 public class HealthController {
+    @Operation(
+            description = "Health check",
+            summary = "Health check",
+            responses = {
+                    @ApiResponse(
+                            description = "Healthy",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description =  "Unhealthy",
+                            responseCode = "500"
+                    )
+            }
+    )
     @GetMapping
     public String health() {
         return "OK";
