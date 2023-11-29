c14ac85763ebc14188d4c50182f2caf367280d77
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-11-29 22:50:42 +0330

    fix service object repository name

--- src/main/java/com/swisscom/tasks/task3/repository/ServiceORepository.java
+++ src/main/java/com/swisscom/tasks/task3/repository/ServiceORepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ServiceRepository extends MongoRepository<ServiceO, String> {
+public interface ServiceORepository extends MongoRepository<ServiceO, String> {
 }
