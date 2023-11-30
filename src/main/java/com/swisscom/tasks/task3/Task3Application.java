7fc908d97cba8d30e9799628bfa727ddc1bcfcf8
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-01 01:09:39 +0330

    Add Redis Cache

--- src/main/java/com/swisscom/tasks/task3/Task3Application.java
+++ src/main/java/com/swisscom/tasks/task3/Task3Application.java
@@ -2,8 +2,12 @@ package com.swisscom.tasks.task3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
+@EnableCaching
 public class Task3Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/swisscom/tasks/task3/configuration/RedisConfiguration.java
@@ -0,0 +1,30 @@
+package com.swisscom.tasks.task3.configuration;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+
+import java.time.Duration;
+
+@AutoConfiguration
+public class RedisConfiguration {
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("itemCache",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
+                .withCacheConfiguration("customerCache",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
+    }
+
+}
