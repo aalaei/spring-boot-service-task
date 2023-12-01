1512c0a07da18161e1419af640711eb3873750a8
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-01 23:08:45 +0330

    Add record support to DTO Mapper

--- src/main/java/com/swisscom/tasks/task3/configuration/DTOMapperBean.java
+++ src/main/java/com/swisscom/tasks/task3/configuration/DTOMapperBean.java
@@ -1,6 +1,7 @@
 package com.swisscom.tasks.task3.configuration;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.record.RecordModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,6 @@ import org.springframework.stereotype.Component;
 public class DTOMapperBean {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        return new ModelMapper().registerModule(new RecordModule());
     }
 }
