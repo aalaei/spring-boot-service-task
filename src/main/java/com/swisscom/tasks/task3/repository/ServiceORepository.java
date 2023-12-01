860ed236de6d1bd2f24fa9f2f6490b57a0ea19d6
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-01 19:12:57 +0330

    Add getIDs to Service Repo

--- src/main/java/com/swisscom/tasks/task3/repository/ServiceORepository.java
+++ src/main/java/com/swisscom/tasks/task3/repository/ServiceORepository.java
@@ -1,9 +1,15 @@
 package com.swisscom.tasks.task3.repository;
 
+import com.swisscom.tasks.task3.dto.service.ServiceIdDTO;
 import com.swisscom.tasks.task3.model.ServiceO;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ServiceORepository extends MongoRepository<ServiceO, String> {
+    @Query(value = "{}", fields = "{id : 1}")
+    List<ServiceIdDTO> findAllIds();
 }
