e45e2a1811965e03b8727678c58371ca1016416d
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-02 21:54:35 +0330

    Disable Cors Filter

--- src/main/java/com/swisscom/tasks/task3/configuration/CorsFilter.java
+++ src/main/java/com/swisscom/tasks/task3/configuration/CorsFilter.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 // Enables CORS for all requests
-@Component
+//@Component
 public class CorsFilter implements Filter {
 
     /**
