847b0c1f26349600f37254d2c5bc229594b3b87d
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-03 19:11:18 +0330

    Add Role Types

--- src/main/java/com/swisscom/tasks/task3/model/auth/Role.java
+++ src/main/java/com/swisscom/tasks/task3/model/auth/Role.java
@@ -3,6 +3,7 @@ package com.swisscom.tasks.task3.model.auth;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 
@@ -10,12 +11,18 @@ import org.springframework.security.core.GrantedAuthority;
 @Document
 @NoArgsConstructor
 public class Role implements GrantedAuthority {
+    public enum RoleType{
+        USER,
+        ADMIN,
+        SUPER_USER,
+    }
     @Id
     private String id;
+    @Indexed(unique = true)
     private String authority;
 
-    public Role(String authority) {
-        this.authority = authority;
+    public Role(RoleType roleType) {
+        this.authority = roleType.name();
     }
 
     @Override
