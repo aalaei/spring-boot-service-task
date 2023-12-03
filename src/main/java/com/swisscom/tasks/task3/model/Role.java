5b0a8e4fc554059dc0f0e8b21815d00d6235ba5c
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-03 12:15:00 +0330

    Role Class Added

---
+++ src/main/java/com/swisscom/tasks/task3/model/Role.java
@@ -0,0 +1,26 @@
+package com.swisscom.tasks.task3.model;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+
+
+@Data
+@Document
+@NoArgsConstructor
+public class Role implements GrantedAuthority {
+    @Id
+    private String id;
+    private String authority;
+
+    public Role(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+}
