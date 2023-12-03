5ffe217e3e3e9887e3a72fa8ed1636f03f403f1a
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-03 18:15:37 +0330

    API Error and Default Exception Handler Added

--- src/main/java/com/swisscom/tasks/task3/exception/ApiError.java
+++ src/main/java/com/swisscom/tasks/task3/exception/ApiError.java
@@ -1,18 +1,15 @@
 package com.swisscom.tasks.task3.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import java.time.LocalDateTime;
 
 /**
- * This class is used to represent an exception in the application api.
+ * This class is used to represent an error in the application api.
  */
-public class ApiException extends RuntimeException {
-    /**
-     * Constructor for ApiException
-     *
-     * @param message - Error message for ApiException
-     */
-    public ApiException(String message) {
-        super(message);
-    }
+public record ApiError(
+    String path,
+    String message,
+    int statusCode,
+    LocalDateTime localDateTime
+){
+
 }
---
+++ src/main/java/com/swisscom/tasks/task3/exception/DefaultExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.swisscom.tasks.task3.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class DefaultExceptionHandler {
+
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    public ResponseEntity<ApiError> handleException(InsufficientAuthenticationException e,
+                                                    HttpServletRequest request) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.FORBIDDEN.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleException(BadCredentialsException e,
+                                                    HttpServletRequest request) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.UNAUTHORIZED.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception e,
+                                                    HttpServletRequest request) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
