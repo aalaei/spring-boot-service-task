ad78450c70bf7801336478ca81f2a01f31d09e52
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-03 18:13:09 +0330

    Clean DTO Objects

--- src/main/java/com/swisscom/tasks/task3/dto/owner/OwnerDTO.java
+++ src/main/java/com/swisscom/tasks/task3/dto/owner/OwnerDTO.java
@@ -1,4 +1,4 @@
-package com.swisscom.tasks.task3.dto;
+package com.swisscom.tasks.task3.dto.owner;
 
 import lombok.Data;
 
--- src/main/java/com/swisscom/tasks/task3/dto/service/ServiceODTONoID.java
+++ src/main/java/com/swisscom/tasks/task3/dto/service/ServiceODTONoID.java
@@ -1,6 +1,6 @@
 package com.swisscom.tasks.task3.dto.service;
 
-import com.swisscom.tasks.task3.dto.OwnerDTO;
+import com.swisscom.tasks.task3.dto.owner.OwnerDTO;
 import com.swisscom.tasks.task3.dto.resource.ResourceDTO;
 import lombok.EqualsAndHashCode;
 
