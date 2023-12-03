16fb6251eda02d3cf9df41a067416ee3b88d09d4
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-03 12:36:20 +0330

    Add dummy security Config

--- src/main/java/com/swisscom/tasks/task3/configuration/SecurityConfig.java
+++ src/main/java/com/swisscom/tasks/task3/configuration/SecurityConfig.java
@@ -1,23 +1,46 @@
 package com.swisscom.tasks.task3.configuration;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.swisscom.tasks.task3.service.MongoUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.crypto.spec.SecretKeySpec;
+
 @Configuration
-//@RequiredArgsConstructor
-@EnableMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
+@EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfig {
+    @Value("${${application.security.jwt.secret-key}}")
+    private String jwtKey;
+    private final MongoUserDetailsService mongoUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
     private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
-            "/v2/api-docs",
             "/v3/api-docs",
             "/v3/api-docs/**",
             "/swagger-resources",
@@ -28,16 +51,55 @@ public class SecurityConfig {
             "/webjars/**",
             "/swagger-ui.html",
             "/graphiql"};
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(WHITE_LIST_URL).permitAll()
+//                        .anyRequest().hasAuthority("SCOPE_READ")
                         .anyRequest().authenticated()
                 )
+                .userDetailsService(mongoUserDetailsService)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(o->o.jwt(Customizer.withDefaults()))
+//                .exceptionHandling(
+//                        (ex) -> ex.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+//                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
+//                .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(){
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getKeyBytes()));
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        SecretKeySpec originalKey= new SecretKeySpec(getKeyBytes(), 0, getKeyBytes().length, "RSA");
+        return NimbusJwtDecoder.withSecretKey(originalKey).macAlgorithm(MacAlgorithm.HS512).build();
+    }
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(List.of("https://localhost:3000"));
+//        configuration.setAllowedHeaders(List.of("*"));
+//        configuration.setAllowedMethods(List.of("GET"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+    @Bean
+    public AuthenticationManager authManager(UserDetailsService userDetailsService) {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(authProvider);
+    }
+
+    private byte[] getKeyBytes(){
+        return jwtKey.getBytes();
     }
 }
