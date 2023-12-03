725ae9b4ecef5a10aae1ac5ca3e1a2aa3e766d4e
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-03 12:57:31 +0330

    Login Request and Response DTO added

---
+++ src/main/java/com/swisscom/tasks/task3/model/LoginResponseDTO.java
@@ -0,0 +1,14 @@
+package com.swisscom.tasks.task3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class LoginResponseDTO {
+    private User user;
+    private String jwt;
+
+}
