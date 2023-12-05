b45738216c74410e29a2b6ab5a052719ac5f4820
ali alaei <ali.alaei.tabatabaei@gmail.com>
2023-12-06 00:00:08 +0330

    Sample dump files are added

--- backup_restore.sh
+++ backup_restore.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Check if redis-dump is installed
-if ! [ -x "$(command -v redis-dump)" ]; then
-    echo 'Error: redis-dump is not installed.' >&2
-    echo 'Installing redis-dump' >&2
-    sudo npm i redis-dump -g
-fi
 
 # check $1 is backup or restore
 if [ "$1" != "backup" ] && [ "$1" != "restore" ]; then
@@ -17,10 +11,13 @@ dump_prefix="dump"
 mkdir -p $dump_prefix
 if [ "$1" == "backup" ]; then
     echo "Backup redis and mongo"
-    redis-dump > $dump_prefix/redis.txt
+    redis-cli save
+    redis-cli --rdb  ${dump_prefix}/dump.rdb
     mongodump --uri=${mongp_url} --gzip --archive=${dump_prefix}/mongo.dump 
 else
     echo "Restore redis and mongo"
     cat $dump_prefix/redis.txt | redis-cli
+    docker cp ${dump_prefix}/dump.rdb task3_redis:/data/dump.rdb
+    # redis-cli shutdown && redis-server --dbfilename dump.rdb --dir /data
     mongorestore --uri="$mongp_url" --gzip --archive=${dump_prefix}/mongo.dump --drop
 fi
